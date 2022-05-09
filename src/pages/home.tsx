882d066fa79d6b6d925fa34c9da351f946749f0c
roneych <roneychristian159@gmail.com>
2022-05-09 17:47:32 -0300

    sidebar component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,19 @@
+import { Flex, SimpleGrid } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { SideBar } from "../components/SideBar";
 
 export default function Home() {
     return (
-        <Header />
+        <Flex direction='column' h='100vh'>
+            <Header />
+
+            <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+                <SideBar />
+
+                <SimpleGrid>
+                        
+                </SimpleGrid>
+            </Flex>
+        </Flex>
     )
 }
