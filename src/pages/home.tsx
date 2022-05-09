3162a95f345d28c6ef4f4188c664a36634dcab8a
roneych <roneychristian159@gmail.com>
2022-05-09 19:05:03 -0300

    users page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,10 @@
-import { Flex, SimpleGrid } from "@chakra-ui/react";
+import { Box, Flex, Icon, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import { faSmileBeam, faSmileWink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { RiEmotionHappyLine } from "react-icons/ri";
 import { Header } from "../components/Header";
 import { SideBar } from "../components/SideBar";
+
 
 export default function Home() {
     return (
@@ -10,8 +14,25 @@ export default function Home() {
             <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
                 <SideBar />
 
-                <SimpleGrid>
+                <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignItems='flex-start'>
+                    <Box
+                        p='8'
+                        bg='gray.800'
+                        borderRadius={8}
                         
+                    >
+                        <Text fontSize='lg' mb='4'>Humor</Text>
+                        <FontAwesomeIcon icon={faSmileBeam} fontSize='100'/>
+                    </Box>
+                    <Box
+                        p='8'
+                        bg='gray.800'
+                        borderRadius={8}
+                        
+                    >
+                        <Text fontSize='lg' mb='4'>Comentário(s) do dia</Text>
+                        
+                    </Box>
                 </SimpleGrid>
             </Flex>
         </Flex>
