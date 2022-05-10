d58a37c60599fdef637fbc1a3f51908f6c0e7f8a
roneych <roneychristian159@gmail.com>
2022-05-10 14:05:53 -0300

    adding signin form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,25 @@ import {
   Stack,
 } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
+import { SubmitHandler, useForm } from 'react-hook-form'
+  
 
- 
+type SignInFormData = {
+  text: string;
+  password: string
+
+}
 
 export default function SignIn() {
+  const { register, handleSubmit, formState} = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values, event) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(values);
+
+  }
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -18,12 +33,13 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <Input name='usuario' type='text' label="Aniversário de namoro(sem '/')"></Input>
-          <Input name='senha' type='senha' label="O que eu mais gosto em você"></Input>
+          <Input name='usuario' type='text' label="Aniversário de namoro(sem '/')" {...register('usuario')}></Input>
+          <Input name='senha' type='password' label="O que eu mais gosto em você" {...register('senha')}></Input>
         </Stack>
-        <Button type="submit" mt="6" colorScheme={"pink"}>
+        <Button type="submit" mt="6" colorScheme={"pink"} isLoading={formState.isSubmitting }>
           Entrar
         </Button>
       </Flex>
