d9ba21f73319d10116b3e744ddaa93c85228f51b
roneych <roneychristian159@gmail.com>
2022-05-11 14:45:18 -0300

    implementing upload image

--- src/pages/fotos.tsx
+++ src/pages/fotos.tsx
@@ -1,40 +1,62 @@
-import { Flex, Input,Button} from "@chakra-ui/react";
+import { Flex, Input, Button, Image, Box, SimpleGrid } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { Header } from "../components/Header";
 import { SideBar } from "../components/Sidebar";
-import { storage } from '../firebase';
-import { ref, uploadBytes, listAll } from 'firebase/storage'
-import { v4 } from 'uuid';
-
-
+import { storage } from "../firebase";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { v4 } from "uuid";
 
 export default function Fotos() {
+  const [imageUpload, setImageUpload] = useState<File>(null);
+  const [imageList, setImageList] = useState<string[]>([]);
 
-    const [imageUpload, setImageUpload] = useState<File>(null); 
-    const [imageList, setImageList] = useState<File[]>([]);
+  const imageListRef = ref(storage, "Fotos/");
+  const uploadImage = () => {
+    if (imageUpload === null) return;
+    const imageRef = ref(storage, `Fotos/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) =>
+      getDownloadURL(snapshot.ref).then((url) =>
+        setImageList((prev) => [...prev, url])
+      )
+    );
+  };
 
-    const uploadImage = () => {
-        if(imageUpload === null) return;
-        const imageRef = ref(storage, `Fotos/${imageUpload.name + v4()}`);
-        uploadBytes(imageRef, imageUpload)
-            .then(() => alert('image uploaded'))
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => setImageList((prev) => [...prev, url]));
+      });
+    });
+  }, []);
 
-    }
-
-    useEffect(() => {
-        listAll()
-
-
-    },[])
-
-
-    return (
-        <><Header />
-        <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <SideBar />
-        <Input type='file' onChange={(e) => {setImageUpload(e.target.files[0])}}/>
-        <Button onClick={uploadImage}>Postar foto</Button>
-        </Flex>
-        </>
-    )
+        <Flex direction='column'>
+            <Box>
+                <Input
+                type="file"
+                onChange={(e) => {
+                    setImageUpload(e.target.files[0]);
+                }}
+                />
+                <Button onClick={uploadImage}>Postar foto</Button>
+            </Box>
+                <Box>
+                {imageList.map((url) => (
+                    <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignItems='flex-start'>
+                        <Box p={['6', '8']}
+                            bg='gray.800'
+                            borderRadius={8}>
+                        <Image src={url} w='sm'/>   
+                        </Box>    
+                    </SimpleGrid>
+                ))}</Box>
+         
+            </Flex>
+            </Flex>
+          </Flex>
+  );
 }
