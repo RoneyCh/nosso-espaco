386ee3d329701a2a55ab7360caf5a0efef436c99
roneych <roneychristian159@gmail.com>
2022-05-13 00:53:59 -0300

    styling comment

--- src/components/Comments/Comment.tsx
+++ src/components/Comments/Comment.tsx
@@ -3,50 +3,51 @@ import React, { useEffect, useState } from "react";
 import { RiDeleteBinLine } from "react-icons/ri";
 import moment from "moment";
 
-
 export default function Comment({ comment, handleDelete, createdAt }) {
   const [newTitle, setNewTitle] = useState<string>(comment.title);
-  const [newOtherTitle, setNewOtherTitle] = useState<string>(comment.otherTitle);
+  const [newOtherTitle, setNewOtherTitle] = useState<string>(
+    comment.otherTitle
+  );
   const [date, setDate] = useState<number>();
   const [dateVv, setDateVv] = useState<number>();
 
   const getDate = async () => {
-      let a =  await createdAt;
-      if(a !== null) {
-        const b = await (a.seconds + (a.nanoseconds/1e+9)) * 1000
-        return b;
-      }
-    
-  }
+    let a = await createdAt;
+    if (a !== null) {
+      const b = (await (a.seconds + a.nanoseconds / 1e9)) * 1000;
+      return b;
+    }
+  };
 
   useEffect(() => {
-    getDate().then(a => {
-      setDate(a); 
+    getDate().then((a) => {
+      setDate(a);
       setDateVv(a);
     });
-  }, [])
-
+  }, []);
 
   return (
     <Box>
-      <Flex justifyContent="space-between" flexWrap='wrap' borderBottom='1px' mb='3'>
-        <Box>
-        <Text m='3' maxW='300'>{comment.title === "" ? newTitle : comment.title}</Text>
-        <Text>{moment(date).fromNow()}</Text>
+      <Flex flexWrap="wrap" borderBottom="1px" mb="3">
+        <Box w='100%'>
+          <Text m="3" maxW="300">
+            {comment.otherTitle === "" ? newOtherTitle : comment.otherTitle}
+          </Text>
+          <Text m="3" maxW="300">
+            {comment.title === "" ? newTitle : comment.title}
+          </Text>
         </Box>
         <Box>
-        <Text m='3' maxW='300'>{comment.otherTitle === "" ? newOtherTitle : comment.otherTitle}</Text>
+          <Text>{moment(date).fromNow()}</Text>
+          <Button
+            colorScheme={"pink"}
+            p="0"
+            height="8"
+            onClick={() => handleDelete(comment.id)}
+          >
+            <RiDeleteBinLine />
+          </Button>
         </Box>
-        <Button
-          colorScheme={"pink"}
-          ml="2"
-          p='0'
-          mt='3'
-          height='8'
-          onClick={() => handleDelete(comment.id)}
-        >
-          <RiDeleteBinLine />
-        </Button>
       </Flex>
     </Box>
   );
